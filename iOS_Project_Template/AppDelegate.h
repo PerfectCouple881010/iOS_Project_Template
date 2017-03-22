068843224c069e3bc931b790b596b59ebcc124c9
张健  ZJ <zhangjian@chinamobo.com>
2017-03-22 15:26:36 +0800

    first commit

---
+++ iOS_Project_Template/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_Project_Template
+//
+//  Created by Summer on 2017/3/22.
+//  Copyright © 2017年 Winter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_Project_Template/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_Project_Template
+//
+//  Created by Summer on 2017/3/22.
+//  Copyright © 2017年 Winter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
